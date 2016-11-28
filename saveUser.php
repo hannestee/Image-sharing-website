c11560e506e4e59292252aad4d083516b014d6af
spongefin <tikkha1@gmail.com>
2016-11-28 13:49:04 +0200

    Update saveUser.php

--- saveUser.php
+++ saveUser.php
@@ -1,31 +1,37 @@
 <?php
-include_once("includes/iheader.php");
-if(isset($_POST["save"])) {
+
+include_once("iheader.php");
+if(isset($_POST["submit"])){
     $datat['username'] = $_POST['username'];
-    $datat['email'] = $_POST['email'];
-    $datat['country'] = $_POST['country'];
-    $datat['password'] = hash('sha256', $_POST['password']. "abc");
-    
-    if(filter_var($_datat['email'], FILTER_VALIDATE_EMAIL)) {
-        try {
-            $stm = $DBH->prepare("INSERT INTO ga_Kayttajatiedot (Kayttajanimi, Salasana, Sposti, Maa) VALUES (:username, :password, :email, :country)");
-            if($stm->execute($datat)){
-                $_SESSION['name'] = $datat['name'];
+    $datat['email'] = $_POST['email'];    
+    $datat['country'] = $_POST['contry'];
+    $datat['password'] = hash('sha256', $_POST['password']."ayy");
+    if(filter_var($datat['email'], FILTER_VALIDATE_EMAIL)) { 
+    try {
+        $stm = $DBH->prepare("INSERT INTO ga_users (username, password, email, country, title) VALUES (:username, :password, :email, :country, 'Newbie')");
+        if($stm->execute($datat)){
+                echo ("aa");
+                $_SESSION['username'] = $datat['username'];
                 $_SESSION['email'] = $datat['email'];
                 $_SESSION['country'] = $datat['country'];
-                $_SESSION['viesti'] = "OK - nyt voit kirjautua " . $_SESSION['email'];
+                $_SESSION['viesti'] = "Voit kirjautua" . $_SESSION['email'];
                 redirect("index.php");
             } else {
-                $_SESSION['viesti'] = "Rekisteröityminen ei onnistu";
+                $_SESSION['viesti'] = "en voi hyvin";
+                echo ("eee");
                 redirect("index.php");
             }
-        } catch(PDOException $e){
-            $_SESSION['viesti'] = "Tietokantaongelma";
-            redirect("index.php");
         }
-    } else {
-        $_SESSION['virhe'] = "Väärä email";
-        redirect("index.php");
+        catch(PDOException $e){
+            $_SESSION['viesti'] = "Tietokantaongelma"; //$e.getMessage()")
+            redirect("index.php");
+            echo ("sss");
+        }
+        } else {
+            echo("Huono sähköposti");
+            $_SESSION['viesti'] = "Väärä email";
+            redirect("index.php");
     }
 }
+
 ?>
