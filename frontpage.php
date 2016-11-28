b7611fddbe7b13511bfa495df9eafcbb54ad4828
spongefin <tikkha1@gmail.com>
2016-11-28 13:10:43 +0200

    Add files via upload

---
+++ frontpage.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<?php
+include("iheader.php");
+?>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+
+    <title>Nettisivu</title>
+    <link rel="stylesheet" type="text/css" href="tyylit.css">
+    <link rel="stylesheet" type="text/css" href="nettisivu_files/dropdown.css">
+</head>
+
+<body>
+    <div class="otsikkowide"></div>
+    <div class="tausta">
+        
+        <div class="otsikko">
+            <div class="dropdown">
+                <span><img class="dropdownpic" src="nettisivu_files/menu2.png"></span>
+                    <div class="dropdown-content">
+                        <a href="https://users.metropolia.fi/~hannest/nettisivu/frontpage.php">Home</a>
+                        <a href="https://users.metropolia.fi/%7Ealeksr/nettisivu/asd">Profile</a>
+                        <a href="https://users.metropolia.fi/%7Ealeksr/nettisivu/asd">Settings</a>
+                        <a href="https://users.metropolia.fi/~hannest/nettisivu/logout.php">Log out</a>
+                    </div>
+            </div>
+            <div class="logo"><a href="https://users.metropolia.fi/~hannest/nettisivu/frontpage.php" target="_self"><img src="nettisivu_files/logo.png" height="60em"></a></div>
+            <img class="profiilikuva" src="nettisivu_files/profiilikuva.jpg">
+        </div>
+
+        <div class="tausta1">
+            
+            <div class="kuvannimi"><p class="normal">Kuvan nimi</p></div> 
+            <div class="latausaika"><p class="normal">Latausaika</p></div> 
+            <img class="clear kuva" src="nettisivu_files/kuva.jpg">
+            
+            <div class="clear kayttajatunnus"><p class="normal">Käyttäjätunnus</p></div>
+            <div class="kommentit"><p class="normal">Kommenttien lkm</p></div>
+            <div class="arvostelu"><p class="normal">Arvostelut</p></div>
+            
+            <div class="hr"><hr></div>
+            
+            <div class="kuvannimi"><p class="normal">Kuvan nimi</p></div> 
+            <div class="latausaika"><p class="normal">Latausaika</p></div> 
+            
+            <img class="kuva" src="nettisivu_files/kuva2.jpg">
+            
+            <div class="kayttajatunnus"><p class="normal">Käyttäjätunnus</p></div>
+            <div class="kommentit"><p class="normal">Kommenttien lkm</p></div>
+            <div class="arvostelu"><p class="normal">Arvostelut</p></div>
+            
+            <div class="hr"><hr></div>
+            
+        </div>
+
+
+
+    </div>
+
+
+</body></html>
