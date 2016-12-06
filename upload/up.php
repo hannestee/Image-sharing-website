5794dfa98d55ae72ca2d89e50fc71386cd1e8c97
spongefin <tikkha1@gmail.com>
2016-12-06 04:26:22 +0200

    Add files via upload

---
+++ upload/up.php
@@ -0,0 +1,96 @@
+
+<?php
+unset($_SESSION['viesti']);
+include_once("../iheader.php");
+
+if ($_SESSION['loggedIn'] == "yes"){
+
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+	$imgName = $_POST['imgName'];
+	$uploaderName = $_POST['username2'];
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+} 
+	
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 50000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+	echo $imgName;
+// if everything is ok, try to upload file
+} else {
+	
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		try {
+			echo "asdasd";
+		$query1 = "SELECT id FROM ga_users WHERE id = '1'";
+		$STH = $DBH->query($query1);
+		$query2 = "SELECT COUNT(*) FROM ga_imgdata";
+		$STH2 = $DBH->query($query2);
+		
+		echo $STH2;
+		echo "asdasd";
+        $stm = $DBH->prepare("INSERT INTO ga_imgdata (url, name, uploadtime) VALUES (:1url, :1name, NOW())"); 
+		$stm->execute(array(
+		"1url" => ($_FILES['fileToUpload']['name']),
+		"1name" => ($imgName)
+		));
+		
+		$datat['id'] = intval($STH);
+		$datat['imgid'] = intval($STH2);
+		$stm2 = $DBH->prepare("INSERT INTO ga_img (uploaderID, imgID) VALUES (:id, :imgid)");
+		if($stm2->execute($datat)){
+                $_SESSION['id'] = $datat['id'];
+		//$stm2->execute(array(
+		//"id" => ($STH)
+		//));	
+		} else {
+                $_SESSION['viesti'] = "en voi hyvin";
+                redirect("../frontpage.php");
+		}
+		}catch(PDOException $e){
+            $_SESSION['viesti'] = "Tietokantaongelma"; //$e.getMessage()")
+		}
+		redirect("../frontpage.php");
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+		
+    }
+}
+} else {
+	$_SESSION['viesti'] = "You aren't logged in";
+	redirect("../frontpage.php");
+	
+}
+
+?>
+
