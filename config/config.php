7dca526ceed4317746347c174c46d9ee3feec290
spongefin <tikkha1@gmail.com>
2016-12-06 04:25:09 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -13,7 +13,7 @@
 //Tietokanta
 $user = 'hannest';        //Käytäjänimi 
 $pass = 'harambe';        //Salasana
-$host = 'localhost';  //Tietokantapalvelin
+$host = 'mysql.metropolia.fi';  //Tietokantapalvelin
 $dbname = 'hannest';        //Tietokanta
 // Muodostetaan yhteys tietokantaan
 try {     //Avataan yhteys tietokantaan ($DBH on nyt luotu yhteysolio, nimi vapaasti valittavissa)
