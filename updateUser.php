d30ae4a5e50f36b8db3c515acfc331938e85ac5d
tomipohj <tomipohj@metropolia.fi>
2016-12-12 14:03:10 +0200

    Update updateUser.php

--- updateUser.php
+++ updateUser.php
@@ -1,41 +1,36 @@
  <?php 
 include_once("iheader.php");
-    if(isset($_GET["save"])){  
+    if(isset($_GET["save"])){ 
+        
     $datat['birthdate'] = $_GET['birthdate'];
     $datat['country'] = $_GET['country'];
     $datat['phonenumber'] = $_GET['phonenumber'];
+    $datat['profilepicurl'] = $_GET['profilepicurl'];
     $datat['username'] = $_SESSION['username2'];
-    try {
         
-        $stm = $DBH->prepare("UPDATE ga_users SET birthdate=:birthdate, country=:country, phonenumber=:phonenumber WHERE username =:username");
+    try {
+        $stm = $DBH->prepare("UPDATE ga_users SET birthdate=:birthdate, country=:country, phonenumber=:phonenumber, profilepicurl=:profilepicurl WHERE username =:username");
         if($stm->execute($datat)){
-                //if(isset($_GET["country"])){  
-                $_SESSION['birthdate'] = $datat['birthdate'];
-                //}
-                //if(isset($_GET["country"])){  
-                $_SESSION['country'] = $datat['country']; 
-                //}
-                //if(isset($_GET["phonenumber"])){ 
-                $_SESSION['phonenumber'] = $datat['phonenumber'];
-                //}
+            $_SESSION['birthdate'] = $datat['birthdate'];
+            $_SESSION['country'] = $datat['country']; 
+            $_SESSION['phonenumber'] = $datat['phonenumber'];
+            $_SESSION['profilepicurl'] = $datat['profilepicurl'];
             redirect("settings.php");
             }
         else {
-                $_SESSION['viesti'] = "en voi hyvin1";
-                //redirect("settings.php");
+            $_SESSION['viesti'] = "en voi hyvin1";
+            redirect("settings.php");
             echo("testi3");
             }
-    }
+        }
         catch(PDOException $e){
             $_SESSION['viesti'] = "Tietokantaongelma"; //$e->getMessage()")
-            //redirect("settings.php");
-            print_r($e);
+            redirect("settings.php");
         }          
     }
-            else {
-                $_SESSION['viesti'] = "en voi hyvin2";
-                echo ("error");
-                //redirect("settings.php");
-            }    
+    else {
+        $_SESSION['viesti'] = "en voi hyvin2";
+        echo ("error");
+        redirect("settings.php");
+    }    
 ?>
-
