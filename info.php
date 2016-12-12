5d8d4bdb3f3b7d3dbdd990ee425caad598b98a5d
spongefin <tikkha1@gmail.com>
2016-12-12 14:40:08 +0200

    Add files via upload

--- info.php
+++ info.php
@@ -58,7 +58,39 @@ include("iheader.php");
                 <div class="infodate">Date</div>
                 
                 <div class="infoimageframe">
-                    <img class="infoimage" src=kuvat/kuva3.jpg>
+					<!--<button type="button" onClick="localStorage.id=<?php echo(5);?>">Click</button>
+					<button onclick="alert(sessionStorage.getItem('id'))">Check Session Value</button> -->
+					<script>
+					//var myPicture = sessionStorage.getItem('id'); 
+					
+									
+					</script>
+					<?php
+					//print_r($picture1);
+					//$picture2 = intval($picture1);
+					//print_r($picture2);
+					
+					
+					$datat['picture'] = $_GET['image'];
+					//print_r($datat);
+					try {
+					$query1 = "SELECT ga_imgdata.url FROM ga_imgdata WHERE ga_imgdata.id = :picture";
+					//print_r($query2);
+		            $STH = $DBH->prepare($query1);
+		            // print_r($STH);
+		            $STH->execute($datat);
+		          	$pictureurl = $STH->fetch();
+		          	//print_r($pictureurl[0]);
+					// print_r("asd");
+                    }catch(PDOException $e) {
+        			echo "Login DB error.";
+        			file_put_contents('log/DBErrors.txt', 'Login: '.$e->getMessage()."\n", 				FILE_APPEND);
+   					}
+					
+           			?>
+			
+					
+                    <img class="infoimage" src="<?php echo("upload/uploads/$pictureurl[0]");?>">
                 </div>
                 
                 <div class="infousername">Username</div>
@@ -82,7 +114,16 @@ include("iheader.php");
             </div>
         </div>
     </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+    </script>
+
+    <!-- <script src="js/main.js"></script> -->
 </body>
+<script type="text/javascript">
+//window.location.href.includes('?image=')
+</script>
 </html>
 
 
